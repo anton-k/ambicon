5dc2cf49d24ae311d680aeaaf610ad5c177fc6ef
anton-k <anton.kholomiov@gmail.com>
2017-04-05 11:58:48 +0300

    read pad-list from directory content

--- ui/Pages/PadList.hs
+++ ui/Pages/PadList.hs
@@ -1,24 +1,45 @@
 module Pages.PadList(padListPage) where
 
+import Data.Ord
+import Data.List
+
 import Dragon.Osc
 
-padListPage _ = Page "pad-list" padUis padKeys
+import ReadDir
 
-padUis =  multiUi (4, 1) (\n -> ui $ Ver [
-        setSend (cmdRun n) $ ui $ VCheck n 8 "olive" (map (("pad " ++) . show) [1 .. 8]) Nothing, 
-        setMsg (flowMsg ("/vol/" ++ show (n + 1)) [Arg 0]) $ ui $ Dial 1.25 "navy" (0, 2)])
+padListPage units = Page "pad-list" (padUis units') padKeys
+    where units' = fmap (sortBy $ comparing fileSrcName) $ fmap snd $ sortBy (comparing fst) units
 
-cmdRun flowIdx = Send [] onValue []
+padUis units =  multiUi (padSize, 1) (\n -> 
+        let len = length $ units !! n
+            names = fmap fileSrcName $ units !! n
+            files = fmap fileSrcPath $ units !! n
+        in ui $ Ver [
+                setSend (cmdRun files n) $ ui $ VCheck (n `mod` len) len "olive" names Nothing, 
+                setMsg (flowMsg ("/vol/" ++ show (n + 1)) [Arg 0]) $ ui $ Dial 1.25 "navy" (0, 2)])
+    where
+        padSize = length units
+
+
+
+cmdRun files flowIdx = Send defaultMsgs onValue []
     where
         onValue = map (\n -> (show n, [                
-                    flowMsg ("/vol/" ++ show (flowIdx + 1)) [ArgFloat 0],
-                    delCmdMsg 0.1 "/process/kill" [ArgString $ "pad-" ++ show flowIdx],
-                    delCmdMsg 0.1 "/process/run" [ArgString $ "pad-" ++ show flowIdx, ArgString $ "csound -+jack_client=pad-" ++ (show $ flowIdx + 1) ++ " test/units/pads/pad-" ++ (show $ 1 + n) ++ ".csd"],
+                    padVol 0,
+                    delCmdMsg 0.1 "/process/kill" [ArgString procName],
+                    delCmdMsg 0.1 "/process/run" [ArgString $ procName, ArgString $ "csound -+jack_client=" ++ jackName ++ " " ++ files !! n],
                     delCmdMsg 0.1 "/run" [ArgString "aj-snapshot -r test/jack-config/flow-test.xml"],
-                    flowMsg ("/vol/" ++ show (flowIdx + 1)) [ArgFloat 1.15]
-                    ])) [0 .. 7]
+                    padVol 1.15
+                    ])) [0 .. len - 1]
 
+        defaultMsgs = [padVol 0, cmdMsg "/process/kill" [ArgString  procName], padVol 1.15]
 
+        procName = "pad-" ++ show flowIdx
+        jackName = "pad-" ++ (show $ flowIdx + 1)
+
+        len = length files
+
+        padVol x = flowMsg ("/vol/" ++ show (flowIdx + 1)) [ArgFloat x]
 
 padKeys = []
 
